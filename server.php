4974be896b12ae95cab3c366a093e29b976ad96e
Chris McCormick <chris@mccormick.cx>
2016-02-29 20:46:42 +0800

    Moved poller into a function.

--- server.php
+++ server.php
@@ -10,33 +10,34 @@ cors();
 // tell PHP it can timeout after 5 minutes
 set_time_limit(300);
 
-$datadir = "data";
+dirPoller("data");
 
-// main loop
-while (true) {
-    // if ajax request has send a timestamp, then $last_ajax_call = timestamp, else $last_ajax_call = null
-    $last_ajax_call = isset($_GET['timestamp']) ? (int)$_GET['timestamp'] : null;
-    // get timestamp of when file has been changed the last time
-    $last_change_in_data_files = dirTimestamp($datadir);
-    // if no timestamp delivered via ajax or data.txt has been changed SINCE last ajax timestamp
-    if ($last_ajax_call == null || $last_change_in_data_files > $last_ajax_call) {
-        // get content of data.txt
-        $data = dirFiles($datadir);
-        // put data.txt's content and timestamp of last data.txt change into array
-        $result = array(
-            'files' => $data,
-            'timestamp' => $last_change_in_data_files
-        );
-        // encode to JSON, render the result (for AJAX)
-        $json = json_encode($result);
-        echo $json;
-        // leave this loop step
-        break;
-    } else {
-        // wait for 1 sec (not very sexy as this blocks the PHP/Apache process, but that's how it goes)
-        sleep( 1 );
-        continue;
-    }
+function dirPoller($datadir) {
+  while (true) {
+      // if ajax request has send a timestamp, then $last_ajax_call = timestamp, else $last_ajax_call = null
+      $last_ajax_call = isset($_GET['timestamp']) ? (int)$_GET['timestamp'] : null;
+      // get timestamp of when file has been changed the last time
+      $last_change_in_data_files = dirTimestamp($datadir);
+      // if no timestamp delivered via ajax or data.txt has been changed SINCE last ajax timestamp
+      if ($last_ajax_call == null || $last_change_in_data_files > $last_ajax_call) {
+          // get content of data.txt
+          $data = dirFiles($datadir);
+          // put data.txt's content and timestamp of last data.txt change into array
+          $result = array(
+              'files' => $data,
+              'timestamp' => $last_change_in_data_files
+          );
+          // encode to JSON, render the result (for AJAX)
+          $json = json_encode($result);
+          echo $json;
+          // leave this loop step
+          break;
+      } else {
+          // wait for 1 sec (not very sexy as this blocks the PHP/Apache process, but that's how it goes)
+          sleep( 1 );
+          continue;
+      }
+  }
 }
 
 // Open a directory, and read its contents
