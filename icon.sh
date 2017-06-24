68bffc33d23611fa90ce69c7a65c4d07691904e8
Chris McCormick <chris@mccormick.cx>
2017-06-24 21:06:51 +0800

    Icon update.

--- icon.sh
+++ icon.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/sh
+#!/bin/sh
 
-font-awesome-svg-png --sizes 192 --color "#1BE3C6" --icons check-circle
+color="#1BE3C6"
+icon=check-circle
+
+font-awesome-svg-png --nopadding --sizes 192 --color "$color" --icons "$icon" --png --dest .
+mv "$color/png/192/$icon.png" resources/public/img/icon.png
+rm -rf "$color"
