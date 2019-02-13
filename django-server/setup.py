c9c28c9c705c675ab3164fbbc6a21482a5008827
Chris McCormick <chris@mccormick.cx>
2019-02-13 21:42:16 +0800

    Add a setup.py so it can be installed with pip.

---
+++ django-server/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='todomini_django_server',
+      version='0.1',
+      description='todoMini Django Server',
+      author='Chris McCormick',
+      author_email='chris@mccormick.cx',
+      url='https://github.com/appMini/todoMini/',
+      packages=['todomini_django_server'],
+     )
