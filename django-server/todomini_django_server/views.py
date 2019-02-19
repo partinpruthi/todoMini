8a0a3deedbf97ed03a84113b47ff7f0a8cf88331
Chris McCormick <chris@mccormick.cx>
2019-02-19 20:26:22 +0800

    Testing streaming poll repsonse direct from gunicorn.

--- django-server/todomini_django_server/views.py
+++ django-server/todomini_django_server/views.py
@@ -1,8 +1,11 @@
 import time
+import json
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
 
-from django.http import JsonResponse
+from django.http import JsonResponse, StreamingHttpResponse
 from django.utils.timezone import make_aware
+from django.db import connection
 
 from .models import TodoFile
 
@@ -43,22 +46,10 @@ def delete_file(request, folder):
 def make_timestamp(dt):
     return time.mktime(dt.timetuple()) + dt.microsecond / 1e6
 
-def dir_poller(request, folder):
-    # http://localhost:8000/server.php?timestamp=1549884871.1449&live_for=30
-    # timestamp: 1549884871.1449
-    # live_for: 30
-    try:
-        timestamp = float(request.GET.get("timestamp", 0))
-    except ValueError:
-        timestamp = 0
-    try:
-        live_for = max(0, min(int(request.GET.get("live_for", 25)), 25))
-    except ValueError:
-        live_for = 25
+def longpoll(folder, live_for, when):
     last_modified = None
-    when = make_aware(datetime.fromtimestamp(timestamp))
-
     while live_for:
+        connection.ensure_connection()
         last_modified = TodoFile.objects.filter(folder=folder).order_by("-modified").first()
         print("dir_poller", folder, last_modified and last_modified.modified)
         if not last_modified:
@@ -66,6 +57,8 @@ def dir_poller(request, folder):
         elif last_modified.modified > when:
             break
         live_for -= 1
+        if connection.connection is not None:
+            connection.close()
         time.sleep(1)
 
     # if we exited the loop early so send the files
@@ -79,11 +72,32 @@ def dir_poller(request, folder):
     else:
         # otherwise just send the last good timestamp
         response = {"timestamp": last_modified and make_timestamp(last_modified.modified) or (time.time() - 1)}
+    
+    return json.dumps(response)
+
+def backgrounder(function, arguments):
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        future = executor.submit(function, *arguments)
+        yield future.result(timeout=60)
+
+def dir_poller(request, folder):
+    # http://localhost:8000/server.php?timestamp=1549884871.1449&live_for=30
+    # timestamp: 1549884871.1449
+    # live_for: 30
+    try:
+        timestamp = float(request.GET.get("timestamp", 0))
+    except ValueError:
+        timestamp = 0
+    try:
+        live_for = max(0, min(int(request.GET.get("live_for", 25)), 25))
+    except ValueError:
+        live_for = 25
+    when = make_aware(datetime.fromtimestamp(timestamp))
 
     # {"timestamp":1549884871.1416}
 
     # {"files":{"Holidays.txt":"[object Object]","Shopping.txt":"[object Object]"},"creation_timestamps":{"Holidays.txt":1549420920,"Shopping.txt":1549420853},"timestamp":1549420920.4828}
 
     # {"files":{"Holidays.txt":"[object Object] * [ ] goober\n * [ ] blar\n","Shopping.txt":"[object Object] * [ ] goober\n"},"creation_timestamps":{"Holidays.txt":1549884871,"Shopping.txt":1549885058},"timestamp":1549885058.6958}
-    return JsonResponse(response)
+    return StreamingHttpResponse(backgrounder(longpoll, (folder, live_for, when)), content_type="application/json")
 
