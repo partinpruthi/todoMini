a50bf14b17cb18884187c583cd937882f9023166
Chris McCormick <chris@mccormick.cx>
2020-04-16 20:03:22 +0800

    Show created, modified, difference, in todofile admin.

--- django-server/todomini_django_server/admin.py
+++ django-server/todomini_django_server/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import TodoFile
 
-admin.site.register(TodoFile)
+class TodoFileAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "created", "modified", "days_used")
+
+    def days_used(self, instance):
+        if instance:
+            return str(instance.modified - instance.created)
+
+admin.site.register(TodoFile, TodoFileAdmin)
